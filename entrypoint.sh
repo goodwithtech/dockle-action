7d129e8d81fec4e99d53d2f9cd551a6a066a22fb
Tomoya AMACHI <tomoya.amachi@gmail.com>
2023-08-18 16:13:40 +0900

    update entrypoint.sh logic for save reports

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,43 +1,62 @@
 #!/bin/sh
-set -e
+set -ex
 
 while getopts ":t:c:l:o:f:" o; do
-   case "${o}" in
-       t)
-         export imageName=${OPTARG}
-         ;;
-       c)
-         export exitCode=${OPTARG}
-         ;;
-       l)
-         export exitLevel=${OPTARG}
-         ;;
-       o)
-         export output=${OPTARG}
-         ;;
-       f)
-         export format=${OPTARG}
-         ;;
-       \?)
-         echo "unknown flag"
-         ;;
+  case "${o}" in
+  t)
+    export imageName=${OPTARG}
+    ;;
+  c)
+    export exitCode=${OPTARG}
+    ;;
+  l)
+    export exitLevel=${OPTARG}
+    ;;
+  o)
+    export output=${OPTARG}
+    ;;
+  f)
+    export format=${OPTARG}
+    ;;
+  \?)
+    echo "unknown flag"
+    ;;
   esac
 done
 
-#shift  $(($OPTIND - 1))
+function run_dockle() {
+  /usr/bin/dockle $@ $imageName
+}
 
-ARGS=""
-if [ $format ];then
- ARGS="$ARGS --format $format"
-fi
-if [ $exitCode ];then
- ARGS="$ARGS --exit-code $exitCode"
-fi
-if [ $exitLevel ];then
- ARGS="$ARGS --exit-level $exitLevel"
-fi
-if [ $output ];then
- ARGS="$ARGS --output $output"
+FIRST_ARGS=""
+SECOND_ARGS=""
+
+if [ $exitLevel ]; then
+  FIRST_ARGS="$FIRST_ARGS --exit-level $exitLevel"
+  SECOND_ARGS="$SECOND_ARGS --exit-level $exitLevel"
 fi
 
-/usr/bin/dockle $ARGS $imageName
+# 1st time: run dockle with exit 0 for left a report file
+if [ $format ]; then
+  FIRST_ARGS="$FIRST_ARGS --format $format"
+fi
+if [ $output ]; then
+  FIRST_ARGS="$FIRST_ARGS --output $output"
+fi
+
+# 2nd time: run dockle with exit code without report file
+if [ $exitCode ]; then
+  SECOND_ARGS="$SECOND_ARGS --exit-code $exitCode"
+fi
+
+RUN_TWICE=""
+if [ "$output" != "" ] && [ "$exitCode" != '0' ]; then
+  RUN_TWICE="TRUE"
+fi
+
+if [ $RUN_TWICE ]; then
+  run_dockle $FIRST_ARGS
+  run_dockle $SECOND_ARGS
+else
+  run_dockle $FIRST_ARGS $SECOND_ARGS
+fi
